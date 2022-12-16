a7418a2082a64f229b928c9d040e9e8478b13632
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-12-16 00:16:36 -0300

    aula-01

---
+++ prisma/migrations/20221216025731_create_notifications/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Notifications" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "recipientId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "readAt" DATETIME,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX "Notifications_recipientId_idx" ON "Notifications"("recipientId");
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notifications.findMany();
+  }
+
+  @Post()
+  create(@Body() body: CreateNotificationBody) {
+    const { category, content, recipientId } = body;
+    return this.prisma.notifications.create({
+      data: {
+        id: randomUUID(),
+        category,
+        content,
+        recipientId,
+      },
+    });
   }
 }
